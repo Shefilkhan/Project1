6527082910ea6cfe82f95b6aefb14c95657842c8
Shefilkhan <spathan8633@conestogac.on.ca>
2025-07-05 18:57:35 -0400

    mention comments for according to SET Coding Standards.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
+/*
+ FILE		: main.cpp	
+ PROGRAMMER	: Shefilkhan Pathan
+ DATE		: 2025-07-05
+ DESCRIPTION: This program calculates the area of a rectangle given its length and breadth.
+*/
+
 #include <stdio.h>
 
+// main function
 int main(void) {
 
+	// Declare variables for length and breadth of the rectangle
 	int Length, Breadth;
 
+	// Prompt the user for input
 	printf("Enter the length of the rectangle: ");
 	scanf_s("%d", &Length);
 
+	// Prompt the user for input
 	printf("Enter the breadth of the rectangle: ");
 	scanf_s("%d", &Breadth);
 
-
+	// Calculate the area of the rectangle
 	int result =  Length * Breadth;
 
+	// Output the result
 	printf("The area of the rectangle is: %d\n", result);
-	return 0;
+
+	return 0; // Return success status
 }
